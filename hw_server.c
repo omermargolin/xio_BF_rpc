d5135118140aa015c8de72b13bc39ccaf76e0133
Igor Yanyutin <Igor.Yanyutin@emc.com>
2019-04-10 17:49:15 +0300

    hw_server.c styling

--- hw_server.c
+++ hw_server.c
@@ -8,7 +8,7 @@
 struct config_t in_config = {
     "mlx5_0",                     /* dev_name */
     NULL,                         /* server_name */
-    INITIAL_PORT,                        /* tcp_port */
+    INITIAL_PORT,                 /* tcp_port */
     1,                            /* ib_port */
     0                             /* gid_idx */
 };
@@ -17,36 +17,32 @@ int test_param=5;
 struct resources resource_handles[MAX_RESOURCE_HANDLES];
 int last_resource_handle = -1;
 
-
-char **hw_1_svc(rpc_args_t *a, struct svc_req *req) {
-	static char msg[256];
-	static char *p;
-	static char *buffer;
-	rpc_args_t* remote_args = (rpc_args_t*)a;
-	int rc;
-	printf ("Entering hw_1_svc\n");
-	printf("getting ready to return value\n");
+char **hw_1_svc(rpc_args_t *remote_args, struct svc_req *req) {
+    static char msg[256];
+    static char *result_p;
+    static char *buffer;
+    // rpc_args_t* remote_args = (rpc_args_t*)a;
+    int rc;
+    printf ("Entering hw_1_svc\n");
+    printf("getting ready to return value\n");
     printf("given queue number=%d\n", remote_args->qp_num);
     printf("src value:%p\n", remote_args->src_add);
     printf("dest value:%p\n", remote_args->dest_add);
     printf("dest key: %x\n", remote_args->dest_key);
-
     printf("address:%p\n", &remote_args->src_add);
-
     printf("device name is:%s\n", remote_args->device_name);
     printf("read from lba:%d\n", remote_args->lba);
     printf("read %d lbas.\n", remote_args->num_lbas);
     printf("write to lba:%d\n", remote_args->dest_lba);
-
 
     buffer = (char *) malloc (remote_args->num_lbas * 512);
     if (!buffer)
     {
        fprintf (stderr, "failed to malloc %Zu bytes to memory buffer\n", remote_args->num_lbas * 512);
        strcpy(msg, "Finish Server");
- 	   p = msg;
- 	   printf("Returning...\n");
-       return (&p);
+        result_p = msg;
+        printf("Returning...\n");
+       return (&result_p);
     }
 
     rc = read_blkdev(remote_args->device_name, remote_args->lba, remote_args->num_lbas, buffer);
@@ -54,35 +50,33 @@ char **hw_1_svc(rpc_args_t *a, struct svc_req *req) {
     {
        fprintf (stderr, "failed to read from device\n");
        strcpy(msg, "Finish Server");
- 	   p = msg;
- 	   printf("Returning...\n");
-       return (&p);
+        result_p = msg;
+        printf("Returning...\n");
+       return (&result_p);
     }
 
     //printf("*address:%s\n", *remote_args->src_add);
+    //     printf("len:%d\n", remote_args->len);
+    //     printf("dest val:%s\n", remote_args->dest_add);
+    //     printf("dest add:%p\n", &remote_args->dest_add);
+    //     printf("dest ref val:%s\n", *remote_args->dest_add);
 
-    //	 printf("len:%d\n", remote_args->len);
-
-    //	 printf("dest val:%s\n", remote_args->dest_add);
-    //	 printf("dest add:%p\n", &remote_args->dest_add);
-	 //	 printf("dest ref val:%s\n", *remote_args->dest_add);
-
-
-	printf("> > > > > Start post_send");
+    printf("> > > > > Start post_send");
     resource_handles[remote_args->qp_num].remote_props.addr =  remote_args->src_add;
-	resource_handles[remote_args->qp_num].remote_buf_len = remote_args->len;
+    resource_handles[remote_args->qp_num].remote_buf_len = remote_args->len;
 
-    	/*
-    	 * Allocate buffer by size of remote len
-    	 */
+    /*
+    * Allocate buffer by size of remote len
+    */
     if (!resource_handles[remote_args->qp_num].buf)
     {
         fprintf (stderr, "failed to malloc %Zu bytes to memory buffer\n", remote_args->len);
         strcpy(msg, "Finish Server");
-  	    p = msg;
-    	printf("Returning...\n");
-        return (&p);
+        result_p = msg;
+        printf("Returning...\n");
+        return (&result_p);
     }
+
     memset (resource_handles[remote_args->qp_num].buf, 0, remote_args->len);
     if (post_send (&resource_handles[remote_args->qp_num], IBV_WR_RDMA_READ))
     {
@@ -103,14 +97,13 @@ char **hw_1_svc(rpc_args_t *a, struct svc_req *req) {
     //Sha2_256(resource_handles[remote_args->qp_num].buf, remote_args->len, &result );
     printf("Result after SHA: ");
     int x;
-    for(x = 0; x < 128; x++)
-      printf("%02x", result[x]);
+    for(x = 0; x < 128; x++) printf("%02x", result[x]);
     putchar( '\n' );
     printf("Result after SHA: 0x%x\n",result);
     printf("Result after SHA: %s\n",result);
     strcpy(msg, "Finish Server");
-    p = msg;
-/*And now return the SHA result*/
+    result_p = msg;
+    /*return the SHA result*/
 
     resource_handles[remote_args->qp_num].remote_props.addr =  remote_args->dest_add;
     uint64_t temp_len =  resource_handles[remote_args->qp_num].remote_buf_len;
@@ -119,8 +112,6 @@ char **hw_1_svc(rpc_args_t *a, struct svc_req *req) {
 
     resource_handles[remote_args->qp_num].remote_props.rkey = remote_args->dest_key;
     printf("rkey: %x, saved rkey\n", resource_handles[remote_args->qp_num].remote_props.rkey ,rkey_temp);
-
-
     sprintf(resource_handles[remote_args->qp_num].buf, result);
     if (post_send (&resource_handles[remote_args->qp_num], IBV_WR_RDMA_WRITE))
     {
@@ -136,41 +127,39 @@ char **hw_1_svc(rpc_args_t *a, struct svc_req *req) {
     sleep(2);
 
     printf("Returning...\n");
-
-    return (&p);
+    return (&result_p);
 }
 
-char **rdmac_1_svc(rpc_args_t *a, struct svc_req *req) {
-	static char msg[256];
-	static char *p;
-//	resources_init (&res);
-	printf("Entering: rmdac_1_svc\n");
-	rpc_args_t* remote_args = (rpc_args_t*)a;
+char **rdmac_1_svc(rpc_args_t *remote_args, struct svc_req *req) {
+    static char msg[256];
+    static char *result_p;
+    //    resources_init (&res);
+    printf("Entering: rmdac_1_svc\n");
+    // rpc_args_t* remote_args = (rpc_args_t*)args;
 
-	printf("getting ready to set rdma connection for id: %d\n", remote_args->qp_num);
+    printf("getting ready to set rdma connection for id: %d\n", remote_args->qp_num);
+    in_config.tcp_port = resource_handles[remote_args->qp_num].tcp_port;
+    create_res_handle(&resource_handles[remote_args->qp_num], in_config);
+    //    printf("getting ready to set global rdma value\n");
+    strcpy(msg, "global changed");
+    /* sprintf(msg, "%d", last_resource_handle); */
+    result_p = msg;
+    printf("rdmac_1_svc: Returning\n", msg);
+    // TODO: create the QP handle like in rdma_queue and store in a global location for hw_1_svc to use when triggered
 
-	in_config.tcp_port = resource_handles[remote_args->qp_num].tcp_port;
-	create_res_handle(&resource_handles[remote_args->qp_num], in_config);
-	//	printf("getting ready to set global rdma value\n");
-	strcpy(msg, "global changed");
-	/* sprintf(msg, "%d", last_resource_handle); */
-	p = msg;
-	printf("rdmac_1_svc: Returning\n", msg);
-	// TODO: create the QP handle like in rdma_queue and store in a global location for hw_1_svc to use when triggered
-
-	return(&p);
+    return(&result_p);
 }
 
-char **aquirep_1_svc(void *a, struct svc_req *req) {
-	static char msg[256];
-	static char *p;
-//	resources_init (&res);
-	printf("Entering: aquirep_svc\n");
-	last_resource_handle++;  // TODO: Add check that we aren't blowing past the end of the array
-	resource_handles[last_resource_handle].tcp_port = INITIAL_PORT + last_resource_handle;
-	sprintf(msg, "%d, %d", last_resource_handle, resource_handles[last_resource_handle].tcp_port);
-//	strcpy(msg, "global changed");
-	p = msg;
-	printf("aquirep eturning %s\n", msg);
-	return(&p);
+char **aquirep_1_svc(void *remote_args, struct svc_req *req) {
+    static char msg[256];
+    static char *result_p;
+//    resources_init (&res);
+    printf("Entering: acquirep_svc\n");
+    last_resource_handle++;  // TODO: Add check that we aren't blowing past the end of the array
+    resource_handles[last_resource_handle].tcp_port = INITIAL_PORT + last_resource_handle;
+    sprintf(msg, "%d, %d", last_resource_handle, resource_handles[last_resource_handle].tcp_port);
+//    strcpy(msg, "global changed");
+    result_p = msg;
+    printf("acquirep returning %s\n", msg);
+    return(&result_p);
 }
